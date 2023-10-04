a6f67f828991dcf5a08b2e7b4513a1b9d325bc61
nabilalansari00 <89952508+nabilalansari00@users.noreply.github.com>
2023-10-03 18:15:55 -0700

    Add files via upload

---
+++ All LTRs from LTRHarvest/neolan1/classvsunclasspie.m
@@ -0,0 +1,20 @@
+
+function f = classvsunclasspie(ltrclassification, ltrname)
+
+percentages = [size(ltrclassification,1)/size(ltrname,1), (size(ltrname,1)-size(ltrclassification,1))/size(ltrname,1)]; % Two percentages: 30% and 70%
+labels = {'Classified', 'Unclassified'}; % Labels for the percentages
+
+figure;
+% Create the pie chart
+f = pie(percentages);
+
+colors = [0.8500 0.3250 0.0980;0 0.4470 0.7410;.5 .5 .5;0.6350 0.0780 0.1840; 0.4940 0.1840 0.5560];
+title('N. lanati')
+colormap(colors);
+
+legend(labels,'Location','northeastoutside')
+set(findobj(f,'type','text'),'fontweight','bold', 'fontsize',14)
+
+
+
+end
---
+++ All LTRs from LTRHarvest/neolan1/graphtranscribedltrs.m
@@ -0,0 +1,72 @@
+function f = graphtranscribedltrs(clusteredltrs_ltrdata)
+
+count= 0; 
+j=1;
+d=1;
+
+
+%enters 0 for rpkm values for cluster rows - needs to be done so the cluster number rows aren't removed
+for i=1:size(clusteredltrs_ltrdata,1)
+    if startsWith(clusteredltrs_ltrdata{i,1},"Cluster")
+        clusteredltrs_ltrdata{i,2} = "0";
+    end
+end
+
+%removes ltrs that aren't transcribed
+emptyStringIndices = strcmp(clusteredltrs_ltrdata{:, 2}, "");
+
+clusteredltrs_ltrdata = clusteredltrs_ltrdata(~emptyStringIndices, :);
+
+
+%removes the cluster headers for the clusters that weren't transcribed
+k=0;
+l=0;
+while k < size(clusteredltrs_ltrdata,1)
+    l=l+1;
+    k=l+1;
+    if startsWith(clusteredltrs_ltrdata{l,'Clusterno_LTRName'},"Cluster") && startsWith(clusteredltrs_ltrdata{k,'Clusterno_LTRName'},"Cluster")
+       clusteredltrs_ltrdata(l,:) = [];
+       l=l-1;
+    end
+end
+
+
+
+%while loop to count number of ltrs in a cluster
+while d < size(clusteredltrs_ltrdata,1)
+    if startsWith(clusteredltrs_ltrdata{d,'Clusterno_LTRName'},"Cluster")
+        count = 0;
+        d=d+1;
+           if d == size(clusteredltrs_ltrdata,1)
+                break;
+            end
+        while startsWith(clusteredltrs_ltrdata{d,'Clusterno_LTRName'},"scaffold")
+            count = count + 1;
+            d=d+1;
+        end
+        ltrsinclustercount(j,1)= count;
+        j=j+1;
+    end
+end
+
+%remove clusters that are of size 1
+ltrsinclustercount(ltrsinclustercount==1) = [];
+
+% % Define the desired bin width
+% binWidth = 2;
+% 
+% % Calculate the number of bins based on the bin width
+% numBins = ceil((max(ltrsinclustercount) - min(ltrsinclustercount)) / binWidth);
+% 
+% % Calculate the custom bin edges
+% binEdges = linspace(min(ltrsinclustercount), max(ltrsinclustercount), numBins + 1);
+
+%graph the data
+figure
+f= histogram(ltrsinclustercount,'facecolor',[0.5, 0.5, 0.5]);
+title('N. lanati')
+xlabel('Cluster Size','fontweight','bold')
+ylabel("Number of Clusters",'fontweight','bold')
+set(gcf,'color','w');
+end
+
